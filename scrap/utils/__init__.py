933f93dc7388c14fcd278eb7af486e6bb79028a7
Adam <adamprogrammer@gmail.com>
2024-11-23 17:10:50 -0700

    Forgot the import at the utils level.

--- scrap/utils/__init__.py
+++ scrap/utils/__init__.py
@@ -4,6 +4,7 @@ import sys
 from .dates import get_monthly_date_range, last_months, recent_quarters
 from .lists import reduce_list
 from .string import cutoff
+from .table import Table
 from .units import humanize_pack_quantity, reduce_dozens, reduce_quantity_by_pack, split_units
 from .uuid import is_valid_uuid
 
